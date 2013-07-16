cdfcf0112bda666871783cd3396acea2d090de5a
Yuriy Vasiyarov <yvasiyarov@gmail.com>
2013-07-17 01:23:54 +0700

    Start working on data source implementation

--- sphinx_agent.go
+++ sphinx_agent.go
@@ -1,17 +1,62 @@
 package main
 
 import (
-	"github.com/yvasiyarov/newrelic_platform_go"
+	"github.com/yunge/sphinx"
+
+//	"github.com/yvasiyarov/newrelic_platform_go"
 )
 
+type SphinxStatusData interface{}
 type MetricsDataSource struct {
+	SphinxHost        string
+	Port              int
+	ConnectionTimeout int
 }
 
+func NewMetricsDataSource(sphinxHost string, port int, connectionTimeout int) *MetricsDataSource {
+	if port == 0 {
+		port = 9312
+	}
+	ds := &MetricsDataSource{
+		SphinxHost:        sphinxHost,
+		Port:              port,
+		ConnectionTimeout: connectionTimeout,
+	}
+	return ds
+}
+
+func (ds *MetricsDataSource) GetData() (*SphinxStatusData, error) {
+	ds.QueryData()
+}
+
+func (ds *MetricsDataSource) QueryData() (*SphinxStatusData, error) {
+	client := sphinx.NewClient().SetServer(ds.SphinxHost, ds.Port)
+
+	if ds.ConnectionTimeout != 0 {
+		client.SetConnectTimeout(ds.ConnectionTimeout)
+	}
+	if err := client.Error(); err != nil {
+		return nil, err
+	}
+
+	defer client.Close()
+	status, err := client.Status()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, row := range status {
+		fmt.Printf("%20s:\t%s\n", row[0], row[1])
+	}
+	return nil, nil
+}
+
+/*
 type Metrica struct {
-    Name string
-    Units string
-    DataKey string
-    Datasource *MetricsDataSource
+	Name       string
+	Units      string
+	DataKey    string
+	Datasource *MetricsDataSource
 }
 
 func (metrica *Metrica) GetName() string {
@@ -27,19 +72,23 @@ func (metrica *WaveMetrica) GetValue() (float64, error) {
 	}
 	return float64(metrica.sawtoothCounter), nil
 }
-
+*/
 
 func main() {
-	plugin := newrelic_platform_go.NewNewrelicPlugin("0.0.1", "7bceac019c7dcafae1ef95be3e3a3ff8866de246", 60)
-	component := newrelic_platform_go.NewPluginComponent("Wave component", "com.exmaple.plugin.gowave")
-	plugin.AddComponent(component)
+	/*
+		plugin := newrelic_platform_go.NewNewrelicPlugin("0.0.1", "7bceac019c7dcafae1ef95be3e3a3ff8866de246", 60)
+		component := newrelic_platform_go.NewPluginComponent("Sphinx component", "com.github.yvasiyarov.Sphinx")
+		plugin.AddComponent(component)
 
-	m := &WaveMetrica{
-		sawtoothMax:     10,
-		sawtoothCounter: 5,
-	}
+		m := &WaveMetrica{
+			sawtoothMax:     10,
+			sawtoothCounter: 5,
+		}
 
-	component.AddMetrica(m)
-	plugin.Verbose = true
-	plugin.Run()
+		component.AddMetrica(m)
+		plugin.Verbose = true
+		plugin.Run()
+	*/
+	ds := NewMetricsDataSource("192.168.0.94", 0, 0)
+	ds.QueryData()
 }
